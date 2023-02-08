7e6ab0933d26ca3b35318be96132462efcfa32be
CJ Montano <cornel.montano@gmail.com>
2023-02-08 09:15:27 -0600

    completed user endpoint w/POST

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     // useNewUrlParser: true, //DEPRECATED, not needed
@@ -18,71 +17,3 @@ const Task = mongoose.model('Task', {
         default: false
     }
 })
-
-const myTask = new Task({
-    description: ' Don\'t worry, Be happy!  ',
-    completed: true
-    //completed: false
-})
-
-myTask.save().then(() => {
-    console.log(myTask)
-}).catch ((error) => {
-    console.log(error)
-})
-
-// const User = mongoose.model('users', {
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     password: {
-//         type: String,
-//         required:true,
-//         trim: true,
-//         minlength: 7,
-//         validate(value) {
-//             if (value.toLowerCase().includes('password')) {
-//                 throw new Error('\'password\' is not a valid selection')
-//             }
-//         }
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         trim:true,
-//         validate(value) {
-//             if (!validator.isEmail(value)) {
-//                 throw new Error('Email is invalid')
-//             }
-//         },
-//         lowercase: true
-//     },
-//     age: {
-//         type: Number,
-//         default: 0,
-//         validate(value) {
-//             if (value < 0) {
-//                 throw new Error('Age must be a positive number')
-//             }
-//         }
-//     }
-// })
-
-// const me = new User({
-//     name: 'CJ',
-//     age: 46,
-//     email: 'Cornel.Montano@gmail.com',
-//     // password: 'rubleduble',
-//     // password: 'short' // Throws error since it is too short
-//     password: 'Password123' // Throws error since not valid
-//     // email: 'joejoe#' // Throws error since not an email
-//     // age: -3 // Throws error defined in model
-//     // age: 'forty six' // Does not work due to validation
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
