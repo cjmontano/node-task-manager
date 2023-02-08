d66b5c0068a0a618b947c6994d98bf8507e387e2
CJ Montano <cornel.montano@gmail.com>
2023-02-08 09:38:55 -0600

    Completed Task Endpoint with POST (forgot index file)

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,16 +4,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     // useNewUrlParser: true, //DEPRECATED, not needed
     // useCreateIndex: true   //DEPRECATED, not needed
 })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        required: false,
-        default: false
-    }
-})
