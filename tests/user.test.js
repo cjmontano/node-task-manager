4c6164b25b909c3bf44b0aafa77259e5c1ece74b
CJ Montano <cornel.montano@gmail.com>
2023-04-10 09:10:39 -0500

    added unit tests | profile fixture

--- tests/user.test.js
+++ tests/user.test.js
@@ -16,6 +16,7 @@ const userOne = {
     }]
 }
 
+// Runs prior to EVERY test
 beforeEach(async () => {
     await User.deleteMany()
     await new User(userOne).save()
@@ -98,4 +99,38 @@ test('Should not delete unauthenticated user', async () => {
     .delete('/users/me')
     .send()
     .expect(401)
+})
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${ userOne.tokens[0].token }`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200)
+
+        const user = await User.findById(userOneId)
+        expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${ userOne.tokens[0].token }`)
+        .send({
+            name: 'Ebers'
+        })
+        .expect(200)
+
+        const user = await User.findById(userOneId)
+        expect(user.name).toBe('Ebers')
+})
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${ userOne.tokens[0].token }`)
+        .send({
+            location: 'Bellevu'
+        })
+        .expect(400)
 })
